6878129abb99a351e9eabe26a1512f8db4e54555
Soleg2008 <Soleg2008g@gmail.com>
2018-04-06 22:58:14 +0300

    bez levoi colonki

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
 $(function () {
 	let windowWidth = $(window).width();
-    if (windowWidth>=768) {
+    if (windowWidth>=992) {
     	$(".header_top_menu").appendTo(".header_top_left");
     	$(".header_top_search").appendTo(".header_top_inner");
     	$(".header_bottom_menu").appendTo(".header_bottom_left_flexItem");
     	$(".header_bottom_basket").appendTo(".header_bottom_inner");
     }
-    if (windowWidth>=992) {
+    if (windowWidth>=1170) {
     	$("img[src='images/logo.png']").attr('src', 'images/logo2.png'); //переопределили src\\
     }
     //выпадающее меню slide toggle переключает дисплей нон-блок\\
     $('.header_burger_right').on("click", function () {
         $(".mobile_menu").stop().slideToggle("slow");
     });
+    $(function() {
+  $('.select').niceSelect();
+});
 });
